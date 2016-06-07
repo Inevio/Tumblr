db6b5f02dad42951077a043f6eab4992862392dd
Jesús Carpintero Rodríguez <j.carpintero@alumnos.upm.es>
2016-06-07 11:14:44 +0200

    Window Size

--- static/script.js
+++ static/script.js
@@ -1,7 +1,21 @@
 
   var win    = $( this );
-  var width  = api.tool.desktopWidth();
-  var height = api.tool.desktopHeight() - 70;
+  var width;
+  var height;
+  var minWidth  = 900;
+  var minHeight = 529;
+  var maxWidth = api.tool.desktopWidth();
+  var maxHeigth = api.tool.desktopHeight() - 70;
+
+
+  if (minHeight > maxHeigth || minWidth > maxWidth) {
+    width = maxWidth;
+    height = maxHeigth;
+
+  }else {
+    width = minWidth;
+    height = minHeight;
+  }
   var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
   var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
   var windowObject = api.popup( 'https://www.tumblr.com/', width, height).render();
